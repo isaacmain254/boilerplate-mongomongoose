e31522436868643de4d41ef766fe0e078bd2b24c
isaacmain254 <isaacmain72@gmail.com>
2023-11-18 21:23:39 +0300

    Modified the findAndUpdate function

--- myApp.js
+++ myApp.js
@@ -62,8 +62,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  findByIdandUpdate(personName, {age:ageToSet}, {new:true}, (err, data) => {
+    if(err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const removeById = (personId, done) => {
